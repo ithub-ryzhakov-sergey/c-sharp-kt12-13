f7f9d9d1113bd1bd4bfc4a904dccb9dc1d835ce7
Danya <dansteamer1337@gmail.com>
2025-10-04 16:51:37 +0300

    asd

--- App/Task1/Task1.cs
+++ App/Task1/Task1.cs
@@ -1,1 +1,31 @@
 namespace App.Task1;
+
+public interface ICache<TKey, TValue>
+{
+    int Capacity { get; }
+    int Count { get; }
+    public bool TryGet(TKey key, out TValue value);
+    public void Set(TKey key, TValue value);
+    public bool ContainsKey(TKey key);
+    public bool Remove(TKey key);
+
+}
+public class LruCache<TKey, TValue> : ICache<TKey, TValue>
+{
+    public TKey key { get; set; }
+    int Capacity { get; }
+    int Count { get; }
+    Dictionary<TKey, TValue> keyValuePairs { get; set; }
+    LruCache(int capacity)
+    {
+        if (capacity < 0) { throw new ArgumentOutOfRangeException(); }
+        else
+        {
+            
+        }
+    }
+    public bool TryGet(TKey key, out TValue value)
+    {
+        return keyValuePairs.ContainsKey(key) && keyValuePairs.ContainsValue(value);
+    }
+}
