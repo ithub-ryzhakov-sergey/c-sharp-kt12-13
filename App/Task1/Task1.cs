868c4bb44826829b9dafbd0cadae9c6a3e322e88
Danya <dansteamer1337@gmail.com>
2025-10-18 10:51:21 +0300

    цук

--- App/Task1/Task1.cs
+++ App/Task1/Task1.cs
@@ -1,31 +1,117 @@
-namespace App.Task1;
 
-public interface ICache<TKey, TValue>
-{
-    int Capacity { get; }
-    int Count { get; }
-    public bool TryGet(TKey key, out TValue value);
-    public void Set(TKey key, TValue value);
-    public bool ContainsKey(TKey key);
-    public bool Remove(TKey key);
+using System;
+using System.Collections.Generic;
 
-}
-public class LruCache<TKey, TValue> : ICache<TKey, TValue>
+namespace App.Task1
 {
-    public TKey key { get; set; }
-    int Capacity { get; }
-    int Count { get; }
-    Dictionary<TKey, TValue> keyValuePairs { get; set; }
-    LruCache(int capacity)
+    public interface ICache<TKey, TValue>
     {
-        if (capacity < 0) { throw new ArgumentOutOfRangeException(); }
-        else
-        {
-            
-        }
+        int Capacity { get; }
+        int Count { get; }
+        bool TryGet(TKey key, out TValue value);
+        void Set(TKey key, TValue value);
+        bool ContainsKey(TKey key);
+        bool Remove(TKey key);
     }
-    public bool TryGet(TKey key, out TValue value)
+
+    public class LruCache<TKey, TValue> : ICache<TKey, TValue>
     {
-        return keyValuePairs.ContainsKey(key) && keyValuePairs.ContainsValue(value);
+        private readonly int _capacity;
+        private readonly Dictionary<TKey, (TValue Value, int Index)> _dictionary;
+        private readonly List<TKey> _accessOrder;
+
+        public int Capacity => _capacity;
+        public int Count => _dictionary.Count;
+
+        public LruCache(int capacity)
+        {
+            if (capacity <= 0)
+                throw new ArgumentOutOfRangeException(nameof(capacity));
+
+            _capacity = capacity;
+            _dictionary = new Dictionary<TKey, (TValue, int)>();
+            _accessOrder = new List<TKey>(capacity);
+        }
+
+        public bool TryGet(TKey key, out TValue value)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            if (_dictionary.TryGetValue(key, out var item))
+            {
+                UpdateAccessOrder(key, item.Index);
+                value = item.Value;
+                return true;
+            }
+
+            value = default(TValue);
+            return false;
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            if (_dictionary.TryGetValue(key, out var existingItem))
+            {
+                UpdateAccessOrder(key, existingItem.Index);
+                _dictionary[key] = (value, _accessOrder.Count - 1);
+            }
+            else
+            {
+                if (Count >= Capacity)
+                {
+                    var lruKey = _accessOrder[0];
+                    _dictionary.Remove(lruKey);
+                    _accessOrder.RemoveAt(0);
+                }
+
+                _accessOrder.Add(key);
+                _dictionary[key] = (value, _accessOrder.Count - 1);
+            }
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            return _dictionary.ContainsKey(key);
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            if (_dictionary.TryGetValue(key, out var item))
+            {
+                _accessOrder.RemoveAt(item.Index);
+                _dictionary.Remove(key);
+
+                for (int i = item.Index; i < _accessOrder.Count; i++)
+                {
+                    var currentKey = _accessOrder[i];
+                    _dictionary[currentKey] = (_dictionary[currentKey].Value, i);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private void UpdateAccessOrder(TKey key, int currentIndex)
+        {
+            _accessOrder.RemoveAt(currentIndex);
+            _accessOrder.Add(key);
+
+            for (int i = currentIndex; i < _accessOrder.Count; i++)
+            {
+                var currentKey = _accessOrder[i];
+                _dictionary[currentKey] = (_dictionary[currentKey].Value, i);
+            }
+        }
     }
 }
